f70fed12a6a7dc0551f4a731fefeff803004879a
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-12-23 13:50:04 +0100

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Homepage</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+        recusandae perspiciatis dignissimos autem fuga facere laborum reiciendis
+        fugit eligendi velit, rerum eum, corrupti voluptates voluptatem ullam in
+        quas at nihil.
+      </p>
+    </div>
+  );
+};
+
+export default Homepage;
