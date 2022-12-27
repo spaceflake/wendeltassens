3784c1f83838e838c4ae6e65cd7ec21c2e2dbfa3
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-12-27 12:18:46 +0100

    create hero component

---
+++ components/Hero.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+import heroImg1 from '../public/hero-Img-1.png';
+
+type Props = {
+  children?: React.ReactNode;
+};
+
+const Hero = ({ children }: Props) => {
+  return (
+    <div className="relative top-0 w-full">
+      <div className=" top-0 relative max-h-[900px] overflow-hidden">
+        <div className="absolute right-1/4 top-1/3">{children}</div>
+        <Image
+          src={heroImg1}
+          alt="hero image"
+          className="w-full object-contain"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
