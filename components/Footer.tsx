20fe56207699b568b01d7d308dcb663a8f155b7f
Olivia Isberg <84776943+OliviaIsberg@users.noreply.github.com>
2022-12-27 11:09:03 +0100

    Footer - changed color on text and added all links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,9 +5,9 @@ type Props = {};
 
 const Footer = (props: Props) => {
   return (
-    <footer className="bg-SuperBrown w-full text-WhiteBG ">
-      <p className="font-Tangerine text-6xl">Wendeltassens</p>
-      <nav>
+    <footer className="bg-SuperBrown text-Beige text-center pl-40 pr-40 pt-40 pb-40">
+      <p className="font-Tangerine text-6xl mb-5">Wendeltassens</p>
+      <nav className="flex justify-between">
         <div className="font-Montserrat flex flex-col">
           <p className="text-lg font-bold">Sitemap</p>
           <Link href="/">Hem</Link>
@@ -29,6 +29,12 @@ const Footer = (props: Props) => {
             <p className="text-lg font-bold">Kattlänkar</p>
             <Link href="https://www.sverak.se/">SVERAK</Link>
             <Link href="http://spinneriet.se">Spinneriet</Link>
+            <Link href="https://scandinavianragdoll.com/">
+              Scandinavien RagdollClub
+            </Link>
+            <Link href="https://ragdollklubben.com/">Ragdollklubben</Link>
+            <Link href="https://stambok.sverak.se/">FindUs</Link>
+            <Link href="http://katter.nu">Katter.nu</Link>
           </div>
         </div>
       </nav>
