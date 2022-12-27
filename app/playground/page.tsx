a266172a21e37799f0bfc2747cbf61efacb6f411
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-12-27 12:18:11 +0100

    add hero to playground

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,12 +1,16 @@
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
+import Hero from '../../components/Hero';
 
 type Props = {};
 
 const page = (props: Props) => {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto relative">
       <Header />
+      <Hero>
+        <h1>Våra Ragdolls</h1>
+      </Hero>
       <div>
         <Footer />
       </div>
