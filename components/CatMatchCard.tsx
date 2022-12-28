554b4bfb2472e6c41e16d45390209f8ad355a43b
Olivia Isberg <84776943+OliviaIsberg@users.noreply.github.com>
2022-12-28 14:46:25 +0100

    CatMatchCard - add media queries for cat match cards

--- components/CatMatchCard.tsx
+++ components/CatMatchCard.tsx
@@ -26,17 +26,21 @@ const CatMatchCard = ({
           alt={name1}
           className="h-[250px] w-[250px] rounded-full object-cover object-top aspect-video"
         />
-        <p className="ml-[3rem]">{name1}</p>
+        <p className="ml-[1rem] md:ml-[3rem] font-Montserrat font-semibold italic">
+          {name1}
+        </p>
       </div>
-      <div className="flex items-center flex-row-reverse">
+      <div className="flex items-center flex-row-reverse mt-[10rem]">
         <Image
           src={catImgUrl2}
           alt={name2}
           className="h-[250px] w-[250px] rounded-full	 object-cover object-top aspect-video"
         />
-        <p className="mr-[3rem]">{name2}</p>
+        <p className="mr-[1rem] md:mr-[3rem]  font-Montserrat font-semibold italic">
+          {name2}
+        </p>
       </div>
-      <div className="flex justify-center">
+      <div className="flex justify-center mt-[5rem]">
         <Button
           text="Stamtavla"
           goTo="https://stambok.sverak.se/Stambok/Visa/506242"
