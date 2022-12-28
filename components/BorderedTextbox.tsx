8812ecb4b0f59046bdd46bd718bbe462e6a94238
Olivia Isberg <84776943+OliviaIsberg@users.noreply.github.com>
2022-12-28 10:39:22 +0100

    BorderAndText - add border color and font on text

--- components/BorderedTextbox.tsx
+++ components/BorderedTextbox.tsx
@@ -7,12 +7,12 @@ type Props = {
 
 const BorderedTextbox = ({ title, text }: Props) => {
   return (
-    <article className="border relative">
+    <article className="border border-DarkBrown rounded relative bg-WhiteBG">
       <h2 className=" font-Tangerine text-5xl absolute top-[-1.5rem] bg-WhiteBG ml-[2rem] pr-[1rem] pl-[1rem]">
         {title}
       </h2>
       <div className="p-[4rem]">
-        <p>{text}</p>
+        <p className="font-Montserrat italic">{text}</p>
       </div>
     </article>
   );
