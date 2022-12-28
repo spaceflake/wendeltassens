406389a17ca74c83148ea1cc8e922a75a7768481
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-12-28 15:59:43 +0100

    add background to kittensection

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -115,7 +115,8 @@ const page = (props: Props) => {
           inverted
         />
       </div>
-      <div className="py-8 flex flex-col items-center space-y-4">
+      <div className="py-[150px] bg-[#80756B]/10 shadow-inner flex flex-col items-center space-y-4">
+        <h2>Tillgängliga kattungar</h2>
         <KittenOverview
           birthDate="2022-11-24"
           father="SE*Mångudinnan Vikings Ragnar"
