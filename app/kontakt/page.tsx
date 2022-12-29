893ea21e6da7244c5be2fff9ab537a6627be5409
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-12-29 15:46:27 +0100

    style contact page

--- app/kontakt/page.tsx
+++ app/kontakt/page.tsx
@@ -1,13 +1,104 @@
+import Hero from '../../components/Hero';
+import Section from '../../components/Section';
+import BorderedTextbox from '../../components/BorderedTextbox';
+
 const Contact = () => {
+  const text =
+    'Hej och välkommen till vårt kontaktformulär! Om du har frågor om att adoptera en ragdoll-kattunge, eller har andra frågor som rör våra djur, vänligen fyll i formuläret nedan så svarar vi så snart vi kan. Tänk på att vara så tydlig och detaljerad som möjligt i din fråga, så att vi kan ge dig den bästa möjliga hjälpen. Vi ser fram emot att höra från dig! Med vänliga hälsningar, Wendeltassens';
   return (
     <div>
-      <h1 className="text-6xl text-DarkBrown font-Tangerine">Kontakt</h1>
-      <p className="text-xl font-Montserrat text-DarkBrown">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
-        recusandae perspiciatis dignissimos autem fuga facere laborum reiciendis
-        fugit eligendi velit, rerum eum, corrupti voluptates voluptatem ullam in
-        quas at nihil.
-      </p>
+      <Hero>
+        <h1>Kontakt & Frågor</h1>
+      </Hero>
+      <Section>
+        <BorderedTextbox title="Frågor?" text={text} />
+      </Section>
+      <Section>
+        <div className="grid grid-cols-1 lg:grid-cols-2 p-12 bg-Beige rounded-xl divide-y lg:divide-x lg:divide-y-0 divide-dotted divide-DarkBrown mb-36">
+          <div className="font-Montserrat text-DarkBrown text-lg pb-10 lg:pr-10 font-bold space-y-4">
+            <p>
+              Går bra att skicka en förfrågan via formuläret bredvid eller så
+              kan du kontakta mig direkt.
+            </p>
+            <div>
+              <p>Liselotte Wendel</p>
+              <p>mail: lise_lotte_1@hotmail.com</p>
+              <p>telefon: 0702040670</p>
+            </div>
+          </div>
+          <form
+            action=""
+            className="flex flex-col text-DarkBrown space-y-4 font-bold pt-10 lg:pl-10 lg:pt-0 mx-auto"
+          >
+            <label htmlFor="name" className="block">
+              <span>Ditt namn</span>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                className="mt-1
+                    block
+                    w-full
+                    rounded-md
+                    bg-Beige
+                    border-transparent
+                    focus:border-DarkBrown focus:bg-WhiteBG focus:ring-0"
+              />
+            </label>
+            <label htmlFor="email" className="block">
+              <span>Din email</span>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="mt-1
+                    block
+                    w-full
+                    rounded-md
+                    bg-Beige
+                    border-transparent
+                    focus:border-DarkBrown focus:bg-WhiteBG focus:ring-0"
+              />
+            </label>
+            <label htmlFor="subject" className="block">
+              <span>Ämne</span>
+              <input
+                type="text"
+                name="subject"
+                id="subject"
+                className="mt-1
+                    block
+                    w-full
+                    rounded-md
+                    bg-Beige
+                    border-transparent
+                    focus:border-DarkBrown focus:bg-WhiteBG focus:ring-0"
+              />
+            </label>
+            <label htmlFor="message" className="block">
+              <span>Ditt meddelande</span>
+              <textarea
+                name="message"
+                id="message"
+                cols={30}
+                rows={7}
+                className="mt-1
+                    block
+                    w-full
+                    rounded-md
+                    bg-Beige
+                    border-transparent
+                    focus:border-DarkBrown focus:bg-WhiteBG focus:ring-0"
+              />
+            </label>
+            <input
+              type="submit"
+              value="Skicka"
+              className="bg-SuperBrown text-Beige py-4 rounded-md"
+            />
+          </form>
+        </div>
+      </Section>
     </div>
   );
 };
--- components/Section.tsx
+++ components/Section.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 
 type Props = {
   children?: React.ReactNode;
+  color?: boolean;
+  rounded?: boolean;
 };
 
-const Section = ({ children }: Props) => {
+const Section = ({ children, color, rounded }: Props) => {
   return (
-    <section className="container mx-auto flex justify-center py-16 lg:py-32">
+    <section
+      className={`container mx-auto flex justify-center pt-16 lg:pt-36 ${
+        color && 'bg-Beige'
+      } ${rounded && 'rounded-xl'}}`}
+    >
       {children}
     </section>
   );
