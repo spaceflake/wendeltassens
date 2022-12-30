a128b7db3c6c8d37b65c7befca96fc13f9a1edbb
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-12-30 11:02:40 +0100

    fix responsiveness on header

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ type Props = {};
 const Header = (props: Props) => {
   return (
     <header className="absolute top-0 w-full z-10 ">
-      <div className="container mx-auto w-full flex flex-col sm:flex-row items-baseline justify-between text-DarkBrown py-4 sm:py-10 bg-WhiteBG/50 backdrop:blur-md px-16 ">
+      <div className="container mx-auto w-full flex flex-col md:flex-row items-center lg:items-baseline justify-between text-DarkBrown py-4 sm:py-10 bg-WhiteBG/50 backdrop:blur-md px-16 ">
         <Link href={'/'}>
           <div className="font-Tangerine text-5xl break-keep mx-auto">
             <span className="text-2xl -mr-3">SE*</span> Wendeltassens
