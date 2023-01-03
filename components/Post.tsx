e2552abe93a1d8f2e977f11240d0334f0b792511
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2023-01-03 14:39:59 +0100

    fix layout on post, now take full width.

--- components/Post.tsx
+++ components/Post.tsx
@@ -12,7 +12,7 @@ const Post = ({ title, text, imgUrl, date }: Props) => {
   const dateFormatted = new Date(date).toISOString().split('T')[0];
 
   return (
-    <article className="bg-WhiteBG rounded-xl overflow-hidden lg:max-h-[480px] lg:max-w-[1000px] flex flex-col-reverse lg:flex-row-reverse shadow-xl">
+    <article className="bg-WhiteBG rounded-xl overflow-hidden lg:max-h-[480px] lg:max-w-[1000px] flex flex-col-reverse lg:flex-row-reverse shadow-xl w-full">
       <div className="px-12 pt-10 pb-12 pl-10 pr-10 grow lg:py-12">
         <div className="space-x-4 text-sm font-thin font-Montserrat text-DarkBrown">
           <span>{dateFormatted}</span>
