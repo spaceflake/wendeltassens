0f476196b217c4d5434925ffb8a528a6c00d6add
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2023-01-05 15:45:32 +0100

    create matchsection component

---
+++ components/MatchSection.tsx
@@ -0,0 +1,36 @@
+import CatMatchCard from './CatMatchCard';
+import Section from './Section';
+import SectionDividerBorder from './SectionDividerBorder';
+
+type Props = {
+  introMatchText: Textblock;
+  matches: Match[];
+};
+
+const MatchSection = ({ introMatchText, matches }: Props) => {
+  return (
+    <>
+      <Section>
+        <SectionDividerBorder title="Planerade kullar" />
+        <p className=" first-letter:text-8xl first-letter:font-Tangerine font-Montserrat font-semibold italic text.xl lg:text-2xl text-center max-w-prose mx-auto mt-10">
+          {introMatchText.text}
+        </p>
+      </Section>
+      <section className="m-auto bg-Beige/50">
+        <div className="mt-16 mb-10 lg:mt-36">
+          {matches.map((match) => (
+            <CatMatchCard
+              key={match._id}
+              femalename={match.motherName}
+              maleName={match.fatherName}
+              femaleImg={match.motherImgUrl}
+              maleImg={match.fatherImgUrl}
+            />
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default MatchSection;
