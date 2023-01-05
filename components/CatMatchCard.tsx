bd418929b1862857b3a4f93de479a5abfd40e11a
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2023-01-05 15:45:09 +0100

    fix catMatchCard, h,w on images

--- components/CatMatchCard.tsx
+++ components/CatMatchCard.tsx
@@ -15,28 +15,32 @@ const CatMatchCard = ({ femalename, maleName, femaleImg, maleImg }: Props) => {
   return (
     <div className="text-DarkBrown py-12 w-full md:w-[40%] md:m-auto">
       <div className="relative">
-        <div className="flex items-start lg:items-center ml-6">
+        <div className="flex items-start ml-6 lg:items-center">
           <Image
             src={femaleImg}
+            height={250}
+            width={250}
             alt={femalename}
             className="h-28 w-28 md:max-lg:h-36 md:max-lg:w-36 xl:h-[250px] xl:w-[250px] rounded-full object-cover object-top aspect-video"
           />
-          <p className="ml-4 md:ml-4 font-Montserrat font-semibold italic">
+          <p className="ml-4 italic font-semibold md:ml-4 font-Montserrat">
             {femalename}
           </p>
         </div>
         <Image
           src={catMatchSvg}
           alt=""
-          className="absolute inset-0 object-contain object-center h-full w-full"
+          className="absolute inset-0 object-contain object-center w-full h-full"
         />
-        <div className="flex items-end lg:items-center flex-row-reverse mr-6">
+        <div className="flex flex-row-reverse items-end mr-6 lg:items-center">
           <Image
             src={maleImg}
+            height={250}
+            width={250}
             alt={maleName}
             className="h-28 w-28 md:max-lg:h-36 md:max-lg:w-36 xl:h-[250px] xl:w-[250px] rounded-full	 object-cover object-top aspect-video"
           />
-          <p className="mr-4 md:mr-4  font-Montserrat font-semibold italic">
+          <p className="mr-4 italic font-semibold md:mr-4 font-Montserrat">
             {maleName}
           </p>
         </div>
