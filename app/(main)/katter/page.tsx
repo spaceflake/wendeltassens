7990bc23528c12f47e43fac040bfe3684973268b
Olivia Isberg <84776943+OliviaIsberg@users.noreply.github.com>
2023-01-05 13:16:05 +0100

    Fix typings on kattsida

--- app/(main)/katter/page.tsx
+++ app/(main)/katter/page.tsx
@@ -5,7 +5,6 @@ import Hero from '../../../components/Hero';
 import Section from '../../../components/Section';
 import SectionDividerBorder from '../../../components/SectionDividerBorder';
 import { client } from '../../../lib/sanity.client';
-import adultCatImg from '../../../public/adult-cat-image-1.png';
 
 const pageQuery = groq`*[_type == "page" && title == "Katter"] {
   heroTitle,
@@ -25,15 +24,15 @@ const pageQuery = groq`*[_type == "page" && title == "Katter"] {
 }`;
 
 const AdultCats = async () => {
-  const pageMeta: Page = await client.fetch(pageQuery);
+  const pageMeta: Page[] = await client.fetch(pageQuery);
 
   const { heroImgUrl, heroTitle } = pageMeta[0];
 
-  const components = pageMeta.map((page) => page.components);
+  const components = pageMeta.map((page) => page.components) as Component[][];
 
-  const textBox = components[0].find(
+  const borderedTextboxText: TextboxBordered = components[0].find(
     (component) => component._type === 'textboxBordered'
-  );
+  ) as TextboxBordered;
 
   const catSections: CatSection[] = components[0].filter(
     (component) => component._type === 'catSection'
@@ -47,8 +46,8 @@ const AdultCats = async () => {
       </Hero>
       <Section>
         <BorderedTextbox
-          title={textBox!.title}
-          text={textBox?.text}
+          title={borderedTextboxText.title}
+          text={borderedTextboxText.text}
         ></BorderedTextbox>
       </Section>
       <Section>
