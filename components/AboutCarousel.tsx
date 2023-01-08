df52acac5e6c8667fff1d48c4add629d95ef2d92
Olivia Isberg <84776943+OliviaIsberg@users.noreply.github.com>
2023-01-08 22:00:47 +0100

    fix image slider on about carousel

--- components/AboutCarousel.tsx
+++ components/AboutCarousel.tsx
@@ -1,28 +1,46 @@
+'use client';
 import Image from 'next/image';
-import { useState } from 'react';
-import heroImg1 from '../public/hero-img-1.png';
+import { useEffect, useState } from 'react';
 
 type Props = {
   carouselImages: string[];
-  children?: React.ReactNode;
 };
 
-const AboutCarousel = ({ carouselImages, children }: Props) => {
+const AboutCarousel = ({ carouselImages }: Props) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const nextIndex = (currentIndex + 1) % carouselImages.length;
+
+  useEffect(() => {
+    const id = setTimeout(() => setCurrentIndex(nextIndex), 10000);
+    return () => clearTimeout(id);
+  }, [currentIndex]);
+
   return (
     <div className="relative top-0 w-full">
-      <div className=" top-0 relative max-h-[900px] overflow-hidden">
-        <div className="absolute right-[15%] top-1/2 flex flex-col">
-          {children}
-        </div>
-        {carouselImages.map((image) => (
-          <Image
-            src={image}
-            width={1920}
-            height={1080}
-            alt="hero image"
-            className="object-contain w-full h-auto"
-          />
-        ))}
+      <div className="top-0 relative aspect-video overflow-hidden">
+        {carouselImages.map((image, index) => {
+          let className =
+            'object-cover w-full h-auto aspect-video absolute top-0';
+
+          if (index === currentIndex) {
+            className += ' animate-carousel-slide-current left-0';
+          } else if (index === nextIndex) {
+            className += ' animate-carousel-slide-next left-full';
+          } else {
+            className += ' left-full';
+          }
+
+          return (
+            <Image
+              key={image}
+              src={image}
+              width={1920}
+              height={500}
+              alt={'Karusell bild'}
+              className={className}
+            />
+          );
+        })}
       </div>
     </div>
   );
