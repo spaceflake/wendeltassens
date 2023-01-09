b4b03b07dbd0d04acab0442e1dc3cde5d5045261
Olivia Isberg <84776943+OliviaIsberg@users.noreply.github.com>
2023-01-09 10:25:52 +0100

    change size on carousel

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,6 +1,7 @@
 'use client';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
+import Section from './Section';
 
 type Props = {
   imageList: string[];
@@ -16,33 +17,35 @@ const Carousel = ({ imageList }: Props) => {
   }, [currentIndex]);
 
   return (
-    <div className="relative top-0 w-full">
-      <div className="top-0 relative aspect-video overflow-hidden">
-        {imageList.map((image, index) => {
-          let className =
-            'object-cover w-full h-auto aspect-video absolute top-0';
+    <Section>
+      <div className="relative top-0 w-full md:w-2/4 m-auto mt-14 sm:mt-32 lg:mt-16">
+        <div className="top-0 relative aspect-video overflow-hidden">
+          {imageList.map((image, index) => {
+            let className =
+              'object-cover w-full h-auto aspect-video absolute top-0';
 
-          if (index === currentIndex) {
-            className += ' animate-carousel-slide-current left-0';
-          } else if (index === nextIndex) {
-            className += ' animate-carousel-slide-next left-full';
-          } else {
-            className += ' left-full';
-          }
+            if (index === currentIndex) {
+              className += ' animate-carousel-slide-current left-0';
+            } else if (index === nextIndex) {
+              className += ' animate-carousel-slide-next left-full';
+            } else {
+              className += ' left-full';
+            }
 
-          return (
-            <Image
-              key={image}
-              src={image}
-              width={1920}
-              height={500}
-              alt={'Karusell bild'}
-              className={className}
-            />
-          );
-        })}
+            return (
+              <Image
+                key={image}
+                src={image}
+                width={1920}
+                height={500}
+                alt={'Karusell bild'}
+                className={className}
+              />
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </Section>
   );
 };
 
