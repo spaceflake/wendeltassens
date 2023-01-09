064f8d8adeb65709a1bc1d6a4bf9c5b2df63f0a4
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2023-01-09 15:31:18 +0100

    fix images on adultcatcard

--- components/AdultCatCard.tsx
+++ components/AdultCatCard.tsx
@@ -9,36 +9,38 @@ type Props = {
 const AdultCatCard = ({ cat, inverted }: Props) => {
   return (
     <article
-      className={`font-Montserrat p-5 lg:p-9 bg-WhiteBG rounded-xl shadow-lg flex flex-col lg:max-h-[500px] w-full max-w-[1000px] ${
+      className={`font-Montserrat p-5 lg:p-9 bg-WhiteBG  rounded-xl shadow-lg flex flex-col lg:max-h-[500px] w-full max-w-[1000px] ${
         inverted ? 'lg:flex-row-reverse' : 'lg:flex-row'
       }`}
     >
-      <Image
-        src={cat.catImgUrl}
-        alt={cat.name}
-        width={500}
-        height={500}
-        className="h-auto w-auto rounded-md grow object-cover object-top aspect-video"
-      ></Image>
-      <div className="p-4 lg:p-8 pb-0 flex flex-col grow">
+      <div className="overflow-hidden">
+        <Image
+          src={cat.catImgUrl}
+          alt={cat.name}
+          height={300}
+          width={300}
+          className="object-contain w-auto h-auto mx-auto rounded-md max-sm:max-h-[200px] "
+        />
+      </div>
+      <div className="flex flex-col p-4 pb-0 lg:p-8 grow">
         <span className="font-thin">{cat.breeder}</span>
-        <h4 className="not-italic text-2xl lg:text-4xl">{cat.name}</h4>
+        <h4 className="text-2xl not-italic lg:text-4xl">{cat.name}</h4>
         <div className="divide-y divide-dashed divide-[#E1DFDE] text-sm text-DarkBrown grow mt-5">
-          <div className="flex justify-between py-2 gap-3">
+          <div className="flex justify-between gap-3 py-2">
             <span className="font-medium text-">Född</span>
             <span className="font-bold text-right">
               {cat.birthDate.split('T')[0]}
             </span>
           </div>
-          <div className="flex justify-between py-2 gap-3">
+          <div className="flex justify-between gap-3 py-2">
             <span className="font-medium text-">Mor</span>
             <span className="font-bold text-right">{cat.motherName}</span>
           </div>
-          <div className="flex justify-between py-2 gap-3">
+          <div className="flex justify-between gap-3 py-2">
             <span className="font-medium text-">Fader</span>
             <span className="font-bold text-right">{cat.fatherName}</span>
           </div>
-          <div className="flex justify-between py-2 gap-3">
+          <div className="flex justify-between gap-3 py-2">
             <span className="font-medium text-">Täckning</span>
             <span className="font-bold text-right">{cat.colorCode}</span>
           </div>
