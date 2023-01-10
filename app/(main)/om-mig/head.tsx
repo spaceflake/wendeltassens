ffbb0ec88879ec03864a33d73fb6e05b8a36da4a
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2023-01-10 11:16:39 +0100

    add meta descriptions to pages

--- app/(main)/om-mig/head.tsx
+++ app/(main)/om-mig/head.tsx
@@ -3,6 +3,10 @@ export default function Head() {
     <>
       <title>Wendeltassens | Om Mig</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        name="description"
+        content="Information om uppfödaren av Wendeltassens Ragdoll."
+      />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
