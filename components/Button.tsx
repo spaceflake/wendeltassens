3fccaa256492fd356cd6a92395a2bfa21618760f
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2023-01-13 11:47:05 +0100

    add secondary option design to Button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,19 +1,45 @@
 import Link from 'next/link';
 
 type Props = {
-  text?: string;
-  goTo?: string;
+  text: string;
+  goTo: string | undefined;
   isExternal?: boolean;
+  secondary?: boolean;
 };
 
-const Button = ({ text, goTo, isExternal }: Props) => {
+const Button = ({ text, goTo, isExternal, secondary }: Props) => {
+  if (secondary) {
+    return (
+      <Link
+        target={isExternal ? '_blank' : '_self'}
+        href={goTo || '/'}
+        className="px-5 py-2 text-sm italic text-center transition-all duration-150 ease-in-out border rounded border-DarkBrown text-DarkBrown hover:ring-2 hover:ring-DarkBrown hover:font-medium font-Montserrat lg:text-base"
+      >
+        {text || 'Knapptext'}
+      </Link>
+    );
+  }
   return (
     <Link
       target={isExternal ? '_blank' : '_self'}
       href={goTo || '/'}
-      className="rounded border border-DarkBrown text-DarkBrown px-5 py-2 hover:bg-DarkBrown hover:text-Beige text-center font-Montserrat italic text-sm lg:text-base"
+      className="flex items-center justify-center gap-2 px-5 py-2 text-sm italic text-center transition-all duration-150 ease-in-out border rounded border-DarkBrown hover:ring-2 hover:ring-DarkBrown hover:font-medium bg-DarkBrown text-Beige font-Montserrat lg:text-base hover:bg-DarkBrown/90"
     >
-      {text || 'Knapptext'}
+      <span>{text || 'Knapptext'}</span>
+      {isExternal && (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-4 h-4 fill-Beige"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M15.75 2.25H21a.75.75 0 01.75.75v5.25a.75.75 0 01-1.5 0V4.81L8.03 17.03a.75.75 0 01-1.06-1.06L19.19 3.75h-3.44a.75.75 0 010-1.5zm-10.5 4.5a1.5 1.5 0 00-1.5 1.5v10.5a1.5 1.5 0 001.5 1.5h10.5a1.5 1.5 0 001.5-1.5V10.5a.75.75 0 011.5 0v8.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V8.25a3 3 0 013-3h8.25a.75.75 0 010 1.5H5.25z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      )}
     </Link>
   );
 };
