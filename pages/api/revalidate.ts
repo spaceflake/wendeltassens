a2028e36fa70b61472be2df224ff01e692025db8
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2023-01-17 10:53:50 +0100

    create revalidate api route

---
+++ pages/api/revalidate.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.query.secret !== process.env.SECRET_TOKEN) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+
+  try {
+    // This should be the actual path not a rewritten path
+    // e.g. for "/blog/[slug]" this should be "/blog/post-1"
+    await res.revalidate('/');
+    await res.revalidate('/katter');
+    await res.revalidate('/kattungar');
+    await res.revalidate('/kontakt');
+    await res.revalidate('/information-villkor');
+    await res.revalidate('/om-mig');
+    return res.json({ revalidated: true });
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send('Error revalidating');
+  }
+}
