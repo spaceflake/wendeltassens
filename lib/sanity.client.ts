14a211f5d45a74f6ba0486ba6b189555c915d8d4
Olivia Isberg <84776943+OliviaIsberg@users.noreply.github.com>
2023-01-22 16:26:09 +0100

    add token to allow creation of documents

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -3,10 +3,12 @@ import { createClient } from 'next-sanity';
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION;
+const token = process.env.NEXT_PUBLIC_SANITY_API_TOKEN;
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
+  token,
   useCdn: false, //'true' if in production
 });
