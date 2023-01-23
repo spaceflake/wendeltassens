af34de4c419d859f0e997e4f0c57ff37a4889753
Olivia Isberg <84776943+OliviaIsberg@users.noreply.github.com>
2023-01-23 11:43:44 +0100

    fix 404 routes

---
+++ app/(main)/[slug]/[...all]/page.tsx
@@ -0,0 +1,10 @@
+import { notFound } from 'next/navigation';
+import React from 'react';
+
+type Props = {};
+
+const page = (props: Props) => {
+  notFound();
+};
+
+export default page;
--- app/(main)/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <div>
-      <h1>Ojdå! Sidan kunde inte hittas.</h1>
-      <h2>404</h2>
-    </div>
-  );
-}
