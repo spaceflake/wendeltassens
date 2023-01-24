09358cc4e4c682c5251dec6ef2e7448c41aaf43a
Olivia Isberg <84776943+OliviaIsberg@users.noreply.github.com>
2023-01-24 16:52:09 +0100

    add open graph

--- app/(main)/[slug]/head.tsx
+++ app/(main)/[slug]/head.tsx
@@ -12,13 +12,15 @@ type Props = {
 const pageQuery = groq`*[_type == "page" && slug.current == $slug][0] {
   title,
   seoDescription,
-  seoKeywords
+  seoKeywords,
+  "seoImage": seoImage.asset->url
 }`;
 
 interface HeadData {
   title: string;
   seoDescription: string;
   seoKeywords: string;
+  seoImage: string;
 }
 
 export default async function Head({ params }: Props) {
@@ -34,6 +36,10 @@ export default async function Head({ params }: Props) {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content={`${data.seoDescription}`} />
       <meta name="keywords" content={`${data.seoKeywords}`} />
+      <meta property="og:title" content={`${data.title}`} />
+      <meta property="og:description" content={`${data.seoDescription}`} />
+      <meta property="og:image" content={`${data.seoImage}`} />
+
       <link rel="icon" href="/favicon.ico" />
     </>
   );
